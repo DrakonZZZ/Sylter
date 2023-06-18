13e8158699eec0538d6807e67d84b45e8a2a85a6
Drakonz <124328116+DrakonZZZ@users.noreply.github.com>
2023-06-18 23:09:23 +0530

    layout

--- src/components/movielist/List.jsx
+++ src/components/movielist/List.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import {
   BsFillArrowRightCircleFill,
   BsFillArrowLeftCircleFill,
@@ -6,17 +7,39 @@ import ListData from '../specificList/ListData';
 import './list.css';
 
 const List = () => {
+  const [slideValue, setSlideValue] = useState(0);
+  const listRef = useRef();
+
+  const handleClick = (transistion) => {
+    let region = listRef.current.getBoundingClientRect().x - 47.9971313477;
+
+    if (transistion === 'transLeft' && slideValue > 0) {
+      setSlideValue(slideValue - 1);
+      listRef.current.style.transform = `translateX(${441 + region}px)`;
+    }
+
+    if (transistion === 'transRight' && slideValue < 8) {
+      setSlideValue(slideValue + 1);
+      listRef.current.style.transform = `translateX(${-441 + region}px)`;
+    }
+
+    console.log(region);
+  };
   return (
     <div className="movie-list">
       <span className="list-title">
         Movies that are being watched<span className="space-color">...</span>
       </span>
       <div className="slider">
-        <BsFillArrowLeftCircleFill className="slider-arrow left" />
-        <BsFillArrowRightCircleFill className="slider-arrow right" />
-        <div className="container">
-          <ListData />
-          <ListData />
+        <BsFillArrowLeftCircleFill
+          className="slider-arrow left"
+          onClick={() => handleClick('transLeft')}
+        />
+        <BsFillArrowRightCircleFill
+          className="slider-arrow right"
+          onClick={() => handleClick('transRight')}
+        />
+        <div className="container" ref={listRef}>
           <ListData />
           <ListData />
           <ListData />
--- src/components/specificList/ListData.jsx
+++ src/components/specificList/ListData.jsx
@@ -1,7 +1,27 @@
+import { AiFillPlayCircle, AiOutlineStar } from 'react-icons/ai';
+import { BiMessageSquareAdd } from 'react-icons/bi';
 import './ListData.css';
 
 const ListData = () => {
-  return <div className="listData">ListData</div>;
+  return (
+    <div className="listData">
+      <img src="https://i.ytimg.com/vi/yKdVYUXyBzU/maxresdefault.jpg" alt="" />
+      <div className="item-info">
+        <AiFillPlayCircle />
+        <BiMessageSquareAdd />
+        <AiOutlineStar />
+      </div>
+      <div className="item-info-selected">
+        <span>1 hour 54mins</span>
+        <span>2022</span>
+      </div>
+      <div className="plot">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
+        facilis nam veniam perspiciatis fugit, commodi quo doloribus, hic id
+      </div>
+      <div className="genre">Thriller</div>
+    </div>
+  );
 };
 
 export default ListData;
